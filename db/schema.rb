ceedb51671a89261d69d2d059030faeafb9e985b
David <david_paixao@estudante.sc.senai.br>
2017-10-25 21:53:55 +0000

    Adicionando state na tabela tasks

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171020000134) do
+ActiveRecord::Schema.define(version: 20171020002244) do
 
   create_table "tasks", force: :cascade do |t|
     t.text     "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer  "user_id"
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
+    t.string   "user_id",    default: "to_do"
+    t.string   "state"
   end
 
   create_table "users", force: :cascade do |t|
