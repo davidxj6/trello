1477752bd89aeeca9cf23c7ba7b32bae7904dd6d
David <david_paixao@estudante.sc.senai.br>
2017-10-26 21:59:25 +0000

    Refatorando as tabelas e add state nos links

--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 20171020002244) do
     t.datetime "created_at",                   null: false
     t.datetime "updated_at",                   null: false
     t.integer  "user_id"
-    t.string   "state"
+    t.string   "state",      default: "to_do"
   end
 
   create_table "users", force: :cascade do |t|
